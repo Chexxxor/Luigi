21822e4738d4d4e08615c10ab1289e8b5415ce53
Chexxxor <chexxor@outlook.com>
2016-03-03 03:17:16 +0100

    Refresh

--- Luigi/src/skillshots/Constants.java
+++ Luigi/src/skillshots/Constants.java
@@ -6,4 +6,5 @@ public interface Constants {
 	int BLOCK_SIZE = 40;
 	int GRID_COLS = WIDTH / BLOCK_SIZE;
 	int GRID_ROWS = HEIGHT / BLOCK_SIZE;
+	int BLOCK_PERCENTAGE = 1;
 }
--- Luigi/src/skillshots/MapGrid.java
+++ Luigi/src/skillshots/MapGrid.java
@@ -12,7 +12,7 @@ public class MapGrid implements Constants {
 	private void generateRandomBlocks(Pane pane){
 		for(int y = 0; y < GRID_ROWS; y++){
 			for(int x = 0; x < GRID_COLS; x++){
-				Color c = (Math.random() * 6 < 2 ? Color.CYAN : Color.BLACK);
+				Color c = (Math.random() * 100 < BLOCK_PERCENTAGE ? Color.CYAN : Color.BLACK);
 				grid[y][x] = new Block(pane, x * BLOCK_SIZE, y * BLOCK_SIZE, c);
 			}
 		}
