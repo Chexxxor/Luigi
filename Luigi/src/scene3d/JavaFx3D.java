55283d712d8248fd2da9f83f4dd1d15e1dbb2202
Chexxxor <chexxor@outlook.com>
2016-03-03 04:35:20 +0100

    Extra files
    Javafx3D

---
+++ Luigi/src/scene3d/JavaFx3D.java
@@ -0,0 +1,81 @@
+package scene3d;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.stage.Stage;
+
+import javafx.scene.PerspectiveCamera;
+
+public class JavaFx3D extends Application {
+	final Group root = new Group();
+	final Group axisGroup = new Group();
+	final Xform world = new Xform();
+	final PerspectiveCamera camera = new PerspectiveCamera(true);
+	final Xform cameraXform = new Xform();
+	final Xform cameraXform2 = new Xform();
+	final Xform cameraXform3 = new Xform();
+	final double cameraDistance = 450;
+
+
+	private void buildScene(){
+		System.out.println("Building Scene...");
+		root.getChildren().add(world);
+	}
+	private void buildCamera() {
+		root.getChildren().add(cameraXform);
+		cameraXform.getChildren().add(cameraXform2);
+		cameraXform2.getChildren().add(cameraXform3);
+		cameraXform3.getChildren().add(camera);
+		cameraXform3.setRotateZ(180.0);
+
+		camera.setNearClip(0.1);
+		camera.setFarClip(10000.0);
+		camera.setTranslateZ(-cameraDistance);
+		cameraXform.ry.setAngle(320.0);
+		cameraXform.rx.setAngle(40);
+	}	
+    private void buildAxes() {
+        System.out.println("buildAxes()");
+        final PhongMaterial redMaterial = new PhongMaterial();
+        redMaterial.setDiffuseColor(Color.DARKRED);
+        redMaterial.setSpecularColor(Color.RED);
+ 
+        final PhongMaterial greenMaterial = new PhongMaterial();
+        greenMaterial.setDiffuseColor(Color.DARKGREEN);
+        greenMaterial.setSpecularColor(Color.GREEN);
+ 
+        final PhongMaterial blueMaterial = new PhongMaterial();
+        blueMaterial.setDiffuseColor(Color.DARKBLUE);
+        blueMaterial.setSpecularColor(Color.BLUE);
+ 
+        final Box xAxis = new Box(240.0, 1, 1);
+        final Box yAxis = new Box(1, 240.0, 1);
+        final Box zAxis = new Box(1, 1, 240.0);
+        
+        xAxis.setMaterial(redMaterial);
+        yAxis.setMaterial(greenMaterial);
+        zAxis.setMaterial(blueMaterial);
+ 
+        axisGroup.getChildren().addAll(xAxis, yAxis, zAxis);
+        world.getChildren().addAll(axisGroup);
+    }
+	@Override
+	public void start(Stage stage) throws Exception {
+		System.out.println("Starting...");
+		buildScene();
+		buildCamera();
+		buildAxes();
+
+		Scene scene = new Scene(root, 1024, 600, true);
+		scene.setFill(Color.GRAY);
+
+		stage.setTitle("My 3D Molecule program");
+		stage.setScene(scene);
+		stage.show();
+		scene.setCamera(camera);
+	}
+}
